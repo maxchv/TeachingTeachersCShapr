0fe5a5738981500b6517a9d51dc20e4eb04d10b3
Maxim <maxshaptala@gmail.com>
2016-02-17 00:15:21 +0200

    Добавлено решение Pentagon

--- FunDriwing/Pentagon/Program.cs
+++ FunDriwing/Pentagon/Program.cs
@@ -20,28 +20,30 @@ namespace Pentagon
         private static void DrawPentagon()
         {
             // Показать черепашку            
-           
+            Turtle.Show();
 
             // Задать максимальную скорость черепашки 10
-           
+            Turtle.Speed = 10;
 
             // Выполнить функцию Draw
-          
+            Draw();
         }
 
         private static void Draw()
         {
             
             // Повторять 200 раз
-           
+            for (int i = 0; i < 200; i++)
+            {
                 // Поменять цвет кисти
-                
+                ColorWeel();
                 // Переместить черепашку на 1 пиксель
-                
+                Turtle.Move(1);
                 // Повернуть черепашку на угол 1/5 от 360 градусов
-                
+                Turtle.Turn(360 / 5);
                 // Повернуть черепашку на угол 1 град                               
-          
+                Turtle.Turn(1);
+            }
         }
         private static string ColorWeel()
         {
