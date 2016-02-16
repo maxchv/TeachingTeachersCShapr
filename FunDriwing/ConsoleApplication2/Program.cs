47cbb30958314fb3e979fcd1f52be22c68ce81c0
Maxim <maxshaptala@gmail.com>
2016-02-17 00:29:12 +0200

    Исправлено задание SpiderNet

--- FunDriwing/ConsoleApplication2/Program.cs
+++ FunDriwing/ConsoleApplication2/Program.cs
@@ -5,15 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace SpiderNet
 {
     // Рисование паутины
     class Program
     {
-        //Объявить переменную length и задать значение 10
+        //Объявить статическую переменную length вещественного типа и задать значение 10
         
-        //Объявить переменную zoom и установить 1        
-
+        //Объявить статическую переменную zoom вещественного типа и установить 1        
+       
         static void Main(string[] args)
         {
             DrawSpiderWeb();
@@ -24,29 +24,31 @@ namespace ConsoleApplication2
             //Установить скорость черепашки равную 10            
             
             //Установить толщину линии 1 пиксель
-
+            GraphicsWindow.PenWidth = 1;
             //Установить цвет линии silver             
-            
+            GraphicsWindow.BrushColor = "Silver";
 
             //Повторять 10 раз
-                     
-                //  Выполнить функцию WeaveOneLayer
+            
+                // Выполнить функцию WeaveOneLayer
+               
+                // изменить значение zoom = zoom * 1.3
                 
-                // изменить значение zoom()*1.3
-                
-           
+            
         }
 
         private static void WeaveOneLayer()
         {
             // Повторять 6 раз            
-            
+            for (int i = 0; i < 6; i++)
+            {
                 // Нарисовать треугольник вызвав функцию DrawTriangle()
-                                
+                
                 // Повернуть черепашку на 1/6 от 360 градусов на право                
                 
-                
                 // Увеличить значение текущей длины линии на текущее значение zoom
+               
+            }
         }
 
         private static void DrawTriangle()
@@ -56,6 +58,8 @@ namespace ConsoleApplication2
                 //  Переместить черепашку на текущую длину length
                 
                 //  Повернуть черепашку на 1/3 от 360 град
+                
+           
         }
     }
 }
